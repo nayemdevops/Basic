65df651df5170120c0a9a9f62cdf90b9d1f4d3b5
Md Ashraful Islam Nayem <nayemdevops@gmail.com>
2024-10-15 12:23:22 -0400

    1st

---
+++ src/JavaBasic/types/variables/DataTypesDemo.java
@@ -0,0 +1,38 @@
+package JavaBasic.types.variables;
+
+public class DataTypesDemo {
+    public static void main(String[] args) {
+
+        //byte
+        byte myByte = 30;
+        System.out.println(myByte);
+
+        //short
+        short myShort = 31;
+
+        //int *****
+        int viewCount = 123_456_789;
+
+        // Long
+        long myLongNumber = 3_123_456_789L;
+
+        //Float ***
+        float shirtPrice = 10.99F;
+        System.out.println(shirtPrice);
+
+        //Double *****
+        double hatPrice = 10.99;
+
+        //Char ***
+        char myPattern = 'A';
+        System.out.println(myPattern);
+
+        //Boolean *****
+        boolean enoughEggStock = true;
+        System.out.println(enoughEggStock);
+
+
+
+
+    }
+}
---
+++ src/JavaBasic/types/variables/VeriablesDemo.java
@@ -0,0 +1,16 @@
+package JavaBasic.types.variables;
+
+public class VeriablesDemo {
+
+    public static void main(String[] args) {
+
+        int myAge = 2;
+        int herAge = myAge;
+        myAge=10;
+        myAge = 200;
+
+        System.out.println(myAge);
+
+
+    }
+}
